ea1c03c553ec0fc29557260fb6c1601a43998d86
Bethwel Njenga <bethwelnjenga237@gmail.com>
2023-11-02 23:07:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
 
   document.querySelectorAll(".drum")[i].addEventListener("click", function() {
 
-    var buttonInnerHTML = this.innerHTML;
-
-    makeSound(buttonInnerHTML);
-
-    buttonAnimation(buttonInnerHTML);
+          var buttonInnerHTML = this.innerHTML;
+      
+          makeSound(buttonInnerHTML);
+      
+          buttonAnimation(buttonInnerHTML);
 
   });
 
@@ -17,9 +17,9 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
 
 document.addEventListener("keypress", function(event) {
 
-  makeSound(event.key);
-
-  buttonAnimation(event.key);
+      makeSound(event.key);
+    
+      buttonAnimation(event.key);
 
 });
 
@@ -28,19 +28,19 @@ document.addEventListener("keypress", function(event) {
 function makeSound(key) {
 
   switch (key) {
-    case "w":
-      var tom1 = new Audio("sounds/tom-1.mp3");
-      tom1.play();
-      break;
-
-    case "a":
-      var tom2 = new Audio("sounds/tom-2.mp3");
-      tom2.play();
-      break;
-
-    case "s":
-      var tom3 = new Audio('sounds/tom-3.mp3');
-      tom3.play();
+          case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+      
+          case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+      
+          case "s":
+            var tom3 = new Audio('sounds/tom-3.mp3');
+            tom3.play();
       break;
 
     case "d":
